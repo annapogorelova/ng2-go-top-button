53a31fabfca008eaebf75cacfcf6ea1b2f4d02c5
Anna Pohorielova <anya.pogorelova@gmail.com>
2017-02-28 22:32:40 +0200

    Added CUSTOM_ELEMENTS_SCHEMA to NgModule to fix rendering errors

--- src/go-top-button.module.ts
+++ src/go-top-button.module.ts
@@ -1,11 +1,12 @@
 import {GoTopButton} from './go-top-button.component';
-import {NgModule} from '@angular/core';
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [GoTopButton],
-    exports: [GoTopButton]
+    exports: [GoTopButton],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
 export class GoTopButtonModule {
