5108a391efeb2aad519c449273b6789bd8282216
Anna Pohorielova <anya.pogorelova@gmail.com>
2017-03-13 17:27:09 +0200

    Removed unneeded CUSTOM_ELEMENTS_SCHEMA

--- src/go-top-button.module.ts
+++ src/go-top-button.module.ts
@@ -1,12 +1,11 @@
 import {GoTopButton} from './go-top-button.component';
-import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 @NgModule({
     imports: [CommonModule],
     declarations: [GoTopButton],
-    exports: [GoTopButton],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    exports: [GoTopButton]
 })
 
 export class GoTopButtonModule {
