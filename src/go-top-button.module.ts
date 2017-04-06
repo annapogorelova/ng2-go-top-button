20f12f7fb6c52520dc1200b0ac4bd7e918f042b1
Anna Pohorielova <anya.pogorelova@gmail.com>
2017-04-06 19:36:46 +0300

    Removed BrowserAnimationsModule from imports.

--- src/go-top-button.module.ts
+++ src/go-top-button.module.ts
@@ -1,10 +1,9 @@
 import {GoTopButton} from './go-top-button.component';
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-    imports: [CommonModule, BrowserAnimationsModule],
+    imports: [CommonModule],
     declarations: [GoTopButton],
     exports: [GoTopButton]
 })
