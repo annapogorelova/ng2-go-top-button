22506261b4b7091d9e6991907dd973c07e5057a9
HARSHITH MULLAPUDI <harshithmullapudi@gmail.com>
2018-09-01 16:25:41 +0530

    Update go-top-button.component.ts

--- src/go-top-button.component.ts
+++ src/go-top-button.component.ts
@@ -16,7 +16,7 @@ import {trigger, state, style, transition, animate} from '@angular/animations';
             position: fixed;
             cursor: pointer;
             outline: none;
-            top: 50%;
+            top: auto;
             bottom: 50%;
             right: 0;
             width: 35px;
